018fa80940e63845cce548847e0aadbbea4c1aa9
RiadBerber-76800 <riadberber@gmail.com>
2022-09-27 12:04:32 +0200

    delete ok

--- resources/views/components/btn-delete.blade.php
+++ resources/views/components/btn-delete.blade.php
@@ -2,7 +2,7 @@
   "post"
 ])
 <div class="">
-  <form action="{{route("posts.destroy", $post->id) }}" method="POST">
+  <form action="{{route("posts.destroy", $post->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this post?')">
     @csrf
     @method("DELETE")
   <button class="btn btn-error">Supprimer</button>
